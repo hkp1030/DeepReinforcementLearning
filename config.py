80ad355699ae02a0980941a2a4184a857deb8bae
hong <fya1030@naver.com>
2021-07-28 01:20:17 +0900

    모델 구조 변경

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
-#### SELF PLAY
+# 자가 경기
 EPISODES = 30
 MCTS_SIMS = 100
 MEMORY_SIZE = 30000
-TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
+TURNS_UNTIL_TAU0 = 10  # 결정론적으로(deterministically) 게임하기 시작하는 때
 CPUCT = 1
 EPSILON = 0.2
 ALPHA = 0.8
 
-
-#### RETRAINING
+# 재학습
 BATCH_SIZE = 256
 EPOCHS = 1
 REG_CONST = 0.0001
@@ -17,14 +16,15 @@ MOMENTUM = 0.9
 TRAINING_LOOPS = 10
 
 HIDDEN_CNN_LAYERS = [
-	{'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	 , {'filters':75, 'kernel_size': (4,4)}
-	]
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+    {'filters': 128, 'kernel_size': (3, 3)},
+]
 
-#### EVALUATION
+# 평가
 EVAL_EPISODES = 20
 SCORING_THRESHOLD = 1.3
